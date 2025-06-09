ef8c708c4bd3c9df9bf682190184ab72c18b8fd2
Yamili Mateo <seleneyamili.bm@gmail.com>
2025-06-09 01:57:08 -0600

    Fixing some errors.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,13 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ListPage from '../views/ListPage.vue'
 import AddPage from '../views/AddPage.vue'
 import EditPage from '../views/EditPage.vue'
-import DeletePage from '../views/DeletePage.vue'
 
 const routes = [
   { path: '/', component: ListPage },
   { path: '/add', component: AddPage },
-  { path: '/edit/:id', component: EditPage, props: true },
-  { path: '/delete', component: DeletePage }
+  { path: '/edit/:id', component: EditPage, props: true }
 ]
 
 const router = createRouter({
